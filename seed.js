679f55068932cbaab5d3cf3af642ce656ec1ed8c
Ciprian Platica <platica.ciprian@gmail.com>
2021-10-26 22:39:04 +0300

    Generate fake stories

---
+++ seed.js
@@ -0,0 +1,23 @@
+const fs = require('fs')
+const faker = require('faker/locale/ro');
+const COUNT = 100;
+
+const entry = () => ({
+  "victimFirstName": faker.name.firstName(),
+  "victimLastName": faker.name.lastName(),
+  "occupation": faker.name.jobTitle(),
+  "age": (Math.floor(Math.random() * 70) + 18).toString(),
+  "content": faker.lorem.paragraphs(),
+  "authorFirstName": faker.name.firstName(),
+  "authorLastName": faker.name.lastName(),
+  "authorEmail": faker.internet.email()
+});
+const jsonString = JSON.stringify([...Array(COUNT).keys()].map(()=>entry()));
+
+fs.writeFile('./import.json', jsonString, err => {
+  if (err) {
+    console.log('Error writing file', err)
+  } else {
+    console.log('Successfully wrote file')
+  }
+})
