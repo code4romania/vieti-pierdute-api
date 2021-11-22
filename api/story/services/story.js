70eccd7fc705706ff4b29d4b2c7595a6d2dfa6ce
Ciprian Platica <platica.ciprian@gmail.com>
2021-11-23 00:47:05 +0200

    Do not return victimLastName if is set private

--- api/story/services/story.js
+++ api/story/services/story.js
@@ -1,8 +1,21 @@
 'use strict';
-
+const { omit } = require('lodash');
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-services)
  * to customize this service
  */
 
-module.exports = {};
+module.exports = {
+  async find(params, populate) {
+    const results = await strapi
+      .query('story')
+      .find(params, populate);
+
+    return results.map((story) => {
+      if (story.hasLastNamePrivate) {
+        return omit(story, 'victimLastName');
+      }
+      return story;
+    });
+  },
+};
--- tests/story/index.js
+++ tests/story/index.js
@@ -1,6 +1,7 @@
 const request = require('supertest');
 const axios = require('axios');
 const { sanitizeEntity } = require('strapi-utils');
+const { omit } = require('lodash');
 const storyFactory = require('./factory');
 const { grantPrivilege } = require('./../helpers/strapi');
 
@@ -32,15 +33,32 @@ it('should find stories', async (done) => {
 });
 
 it('should not return lastName if hasLastNamePrivate is true', async (done) => {
-  await storyFactory.createStory({
-    hasLastNamePrivate: true,
-  });
+  const requestBody = storyFactory.mockStoryData();
+  const expectedResponse = sanitizeEntity(
+    omit(
+      {
+        id: 1,
+        ...requestBody,
+      },
+      'victimLastName'
+    ),
+    { model: strapi.models.story }
+  );
   await request(strapi.server)
-    .get('/stories')
-    .expect(200)
-    .then((data) => {
-      expect(data.text).toBe('[]');
-      done();
+    .post('/stories')
+    .send({
+      ...requestBody,
+      hasLastNamePrivate: true,
+      recaptcha: 'recaptcha code',
+    })
+    .then(async () => {
+      await request(strapi.server)
+        .get('/stories')
+        .expect(200)
+        .then(async (data) => {
+          expect(data.text).toBe(JSON.stringify([expectedResponse]));
+          done();
+        });
     });
 });
 
@@ -48,7 +66,7 @@ it('should create story if data and recaptcha are valid', async (done) => {
   const requestBody = storyFactory.mockStoryData();
   const expectedResponse = sanitizeEntity(
     {
-      id: 1,
+      id: 2,
       ...requestBody,
       image: null,
     },
@@ -62,24 +80,49 @@ it('should create story if data and recaptcha are valid', async (done) => {
       recaptcha: 'recaptcha code',
     })
     .expect(200)
-    .then((data) => {
+    .then(async (data) => {
       expect(data.text).toBe(JSON.stringify(expectedResponse));
-      done();
+
+      await request(strapi.server)
+        .get('/stories')
+        .expect(200)
+        .then(async (data) => {
+          expect(data.text).toBe(
+            JSON.stringify([
+              {
+                id: 1,
+                ...omit(requestBody, [
+                  'authorFirstName',
+                  'authorLastName',
+                  'authorEmail',
+                  'hasLastNamePrivate',
+                  'victimLastName',
+                ]),
+              },
+              expectedResponse,
+            ])
+          );
+          done();
+        });
     });
 });
 
 it('should throw error when try to create story with no data', async (done) => {
-  const expectedResponse = storyFactory.badRequest({
-    victimFirstName: ['victimFirstName must be defined.'],
-    victimLastName: ['victimLastName must be defined.'],
-    occupation: ['occupation must be defined.'],
-    content: ['content must be defined.'],
-    authorFirstName: ['authorFirstName must be defined.'],
-    authorLastName: ['authorLastName must be defined.'],
-    authorEmail: ['authorEmail must be defined.'],
-    county: ['county must be defined.'],
-    city: ['city must be defined.'],
-  });
+  const expectedResponse = storyFactory.badRequest(
+    {
+      victimFirstName: ['victimFirstName must be defined.'],
+      // victimLastName: ['victimLastName must be defined.'],
+      occupation: ['occupation must be defined.'],
+      content: ['content must be defined.'],
+      authorFirstName: ['authorFirstName must be defined.'],
+      authorLastName: ['authorLastName must be defined.'],
+      authorEmail: ['authorEmail must be defined.'],
+      authorRelation: ['authorRelation must be defined.'],
+      county: ['county must be defined.'],
+      city: ['city must be defined.'],
+    },
+    'ValidationError'
+  );
 
   await request(strapi.server)
     .post('/stories')
@@ -95,9 +138,12 @@ it('should throw error when try to create story with no data', async (done) => {
 
 it('should throw error if captcha code is missing', async (done) => {
   const requestBody = storyFactory.mockStoryData();
-  const expectedResponse = storyFactory.badRequest({
-    recaptcha: ['Captcha must be defined.'],
-  });
+  const expectedResponse = storyFactory.badRequest(
+    {
+      recaptcha: ['Captcha must be defined.'],
+    },
+    'FailedCaptchaScore'
+  );
 
   await request(strapi.server)
     .post('/stories')
@@ -113,9 +159,12 @@ it('should throw error if captcha code is missing', async (done) => {
 
 it('should throw error if captcha code has expired', async (done) => {
   const requestBody = storyFactory.mockStoryData();
-  const expectedResponse = storyFactory.badRequest({
-    recaptcha: ['Recaptcha score failed.'],
-  });
+  const expectedResponse = storyFactory.badRequest(
+    {
+      recaptcha: ['Recaptcha score failed.'],
+    },
+    'FailedCaptchaScore'
+  );
 
   axios.post.mockResolvedValue({ data: { success: false } });
 
@@ -134,11 +183,14 @@ it('should throw error if captcha code has expired', async (done) => {
 
 it("should throw error if google link doesn't work", async (done) => {
   const requestBody = storyFactory.mockStoryData();
-  const expectedResponse = storyFactory.badRequest({
-    recaptcha: [
-      'Recaptcha failed, please try again in a few minutes.',
-    ],
-  });
+  const expectedResponse = storyFactory.badRequest(
+    {
+      recaptcha: [
+        'Recaptcha failed, please try again in a few minutes.',
+      ],
+    },
+    'FailedCaptchaScore'
+  );
 
   axios.post.mockReturnValue(() => {
     throw new Error();
