1b902b15f353d2462c79de557e22cccb0ebc2cd0
Răzvan Pavel <razvan.e.pavel@gmail.com>
2021-11-26 09:28:33 +0200

    disable cache

--- config/middleware.js
+++ config/middleware.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   settings: {
     cache: {
-      enabled: true,
+      enabled: false,
       populateContext: true,
       clearRelatedCache: true,
       models: ['banners', 'pages', 'stories']
